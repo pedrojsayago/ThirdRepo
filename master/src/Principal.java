af661ea7f15a8fd47ffa45590977c51a6e63fe37
Pedro Sayago <pjps2002@hotmail.com>
2013-01-22 23:07:24 +0000

    adding audit, archetype and persistence

--- master/src/Principal.java
+++ master/src/Principal.java
@@ -1,11 +1,6 @@
 import swing.VentanaSwingLectorNotas;
 
 /**
- * Created with IntelliJ IDEA.
- * User: pedro
- * Date: 22/01/13
- * Time: 21:45
- * To change this template use File | Settings | File Templates.
  */
 public class Principal {
     public static void main (String [] args) {
